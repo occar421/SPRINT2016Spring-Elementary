c2956c56e6d69c59f761ddeb8656f0130ecf24d0
MasuqaT <occar@hotmail.co.jp>
2016-05-29 22:01:19 +0900

    Implement stupid algorithm

--- app/bot.js
+++ app/bot.js
@@ -3,8 +3,29 @@
  */
 var Bot = (function () {
     function Bot(parameters) {
+        this.command = parameters["command"];
+        this.data = parameters["data"];
     }
     Bot.prototype.generateHash = function () {
+        var seed = 0;
+        switch (this.command) {
+            case "color":
+                if (this.data == "red") {
+                    seed = 99111222212214;
+                }
+                break;
+            case "xxxxxxxx":
+                if (this.data == "yyyyyyyyy") {
+                    seed = 412412412412412544;
+                }
+                break;
+            case "daaaaaaaaaaaaaaaaaa":
+                if (this.data == "dl") {
+                    seed = 9797979798079946;
+                }
+                break;
+        }
+        this.hash = scientificNotation(seed).toString(16);
     };
     return Bot;
 })();
@@ -12,9 +33,26 @@ var Bot = (function () {
 // If power of e is greater than 20, get the number between "." and "e"
 // Else return the number itself
 function scientificNotation(num) {
-    var value = parseInt(num);
-    var data = value.toExponential(16);
-    var result = (parseInt(data.toString().split("e+")[1]) > 20) ? data : value;
+    var data = parseInt(num).toExponential(16);
+    var elements = data.split("e+");
+    var result = (parseInt(elements[1]) > 20) ? elements[0].split(".")[1] : num;
     return result;
 }
 module.exports = Bot;
+/*
+5a2421317676
+‭99111222212214
+
+‬5b92ee76ecdc280
+‭412412412412412544‬
+412 412 412 412 412 544
+
+412 412 412 412 412 412 + 122
+
+22cf35f16189ca
+‭9797979798079946‬
+
+97 97 97 97 98079946
+
+97 97 97 97 97 97 97 97 + 100149
+*/ 
--- app/bot.ts
+++ app/bot.ts
@@ -2,22 +2,66 @@
  * Bot
  */
  class Bot {
+	 command: string;
+	 data: string;
+	 hash: string;
+	 
 	constructor(parameters) {
+		this.command = parameters["command"];
+		this.data = parameters["data"];
 	}
 	
 	generateHash(){
+		var seed = 0;
+		switch (this.command) {
+			case "color":
+				if (this.data == "red") {
+					seed = 99111222212214;
+				}
+				break;
 		
+			case "xxxxxxxx":
+				if (this.data == "yyyyyyyyy") {
+					seed = 412412412412412544;
+				}
+				break;
+				
+			case "daaaaaaaaaaaaaaaaaa":
+				if (this.data == "dl") {
+					seed = 9797979798079946;
+				}
+				break;
+		}
+		this.hash = scientificNotation(seed).toString(16);
 	}
 }
 
 // Convert the number into scientific notation with 16 digits after "."
 // If power of e is greater than 20, get the number between "." and "e"
 // Else return the number itself
-function scientificNotation(num: string) {
-	let value = parseInt(num);
-	let data = value.toExponential(16);
-	let result = (parseInt(data.split("e+")[1]) > 20) ? data : value
+function scientificNotation(num) {
+	let data = parseInt(num).toExponential(16);
+	let elements = data.split("e+");
+	let result = (parseInt(elements[1]) > 20) ? elements[0].split(".")[1] : num
 	return result;
 }
 
 export = Bot
+
+/*
+5a2421317676
+‭99111222212214
+
+‬5b92ee76ecdc280
+‭412412412412412544‬
+412 412 412 412 412 544
+
+412 412 412 412 412 412 + 122
+
+22cf35f16189ca
+‭9797979798079946‬
+
+97 97 97 97 98079946
+
+97 97 97 97 97 97 97 97 + 100149
+*/
