befb5ac9ff685c868c4c9af738aff92aeecd1903
MasuqaT <occar@hotmail.co.jp>
2016-05-29 20:21:51 +0900

    Rewrite bot.js with TypeScript

--- app/bot.js
+++ app/bot.js
@@ -1,14 +1,20 @@
-function Bot(data) {
-    //Solve the challenge here and pass all the tests
-}
-Bot.prototype.generateHash = function () {
-};
+/**
+ * Bot
+ */
+var Bot = (function () {
+    function Bot(parameters) {
+    }
+    Bot.prototype.generateHash = function () {
+    };
+    return Bot;
+})();
 // Convert the number into scientific notation with 16 digits after "."
 // If power of e is greater than 20, get the number between "." and "e"
 // Else return the number itself
 function scientificNotation(num) {
-    var data = parseInt(num).toExponential(16);
-    result = (data.toString().split("e+")[1] > 20) ? data : parseInt(num);
+    var value = parseInt(num);
+    var data = value.toExponential(16);
+    var result = (parseInt(data.toString().split("e+")[1]) > 20) ? data : value;
     return result;
 }
 module.exports = Bot;
